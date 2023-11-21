74c38286a6e7a01cd3c9d7237f593582b290e137
akhil <akhiljawali91@gmail.com>
2023-11-21 16:14:11 +0530

    contact us pop dontr

--- index.js
+++ index.js
@@ -8,3 +8,23 @@ function dropDownAnswer(element) {
         element.parentElement.classList.remove('open');
     }
 }
+
+function contactUs() {
+    var name = document.getElementById("name");
+    var mail = document.getElementById("mail");
+    var ContactNo = document.getElementById("ContactNo");
+    var message = document.getElementById("message");
+    let isError = false;
+
+    var contactInfo = [name, mail, ContactNo, message];
+    for (let i = 0; i < contactInfo.length; i++) {
+        if (contactInfo[i].value == "") {
+            contactInfo[i].style.border = "1px solid red"
+            isError = true;
+            // return false;
+        }
+    }
+}
+function removeErrorBorder(id) {
+    document.getElementById(id).style.border = 'none'
+}
